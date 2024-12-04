61ec0c30e2bf27d10a440638f793765424b102e2
awdev <165525907+awdev1@users.noreply.github.com>
2024-12-04 07:51:51 -0800

    ok yeah no im tired - 6:43 am aaron

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpUntst.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpUntst.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
