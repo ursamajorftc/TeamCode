ac4b7119ae2f7178395ea989c3d460fe984a614d
awdev <165525907+awdev1@users.noreply.github.com>
2024-12-07 08:08:02 -0800

    oh man overnighter no sleep and 2 crashed out men

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProdTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProdTeleOP.java
@@ -8,12 +8,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import java.util.concurrent.locks.Lock;
 
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "EXPDASHTeleOp", group = "Linear OpMode")
@@ -21,14 +17,40 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class ProdTeleOP extends LinearOpMode {
 
     private DcMotor intakeDrive;
+    private DcMotor outtakeDrive1;
+    private DcMotor outtakeDrive2;
+
     private Servo intakeServoLeft;
     private Servo intakeServoRight;
     private CRServo intakeCRSLeft;
     private CRServo intakeCRSRight;
-    private Servo clawServo;
-    private Servo wristServo;
     private Servo armServo;
-    private static final double DISTANCE_THRESHOLD_MM = 12.0;  // Threshold in mm
+    private Servo fingerServo;
+
+    private Servo wristServo;
+    private Servo lockServo;
+
+
+    @Override
+    public void resetRuntime() {
+        super.resetRuntime();
+    }
+
+
+    public static double ARMGRAB = -0.3; // arm at 90 degrees paralell with the intake
+    public static double WRIST90 = 0.5; // pos for graby graby block
+    public static double FINGEROPEN = 0.4; // pos for open finger
+    public static double FINGERCLOSE = 0; // pos for close finger
+    public static int ARMVERTICAL = 0; // VERTICAL WITH RAILS
+    //raise the rails
+    public static double WRISTSCORE = -0.5; // funny angle for the drop
+    // and then finger opens
+    public static double VERTDOWNPOWER1 = 0.5;
+    public static double VERTDOWNPOWER2 = -0.5;
+    public static double VERTDOWNGO1 = 0;
+    // these should be default i think
+    public static double VERTDOWNGO2 = 0;
+
     public static int FULL_EXTENSION = 965;
     public static int HALF_EXTENSION = 482;
     public static int QUARTER_EXTENSION = 241;
@@ -37,12 +59,11 @@ public class ProdTeleOP extends LinearOpMode {
     public static double INTAKE_UP_LPOSITION = 0.18;
     public static double INTAKE_UP_RPOSITION = 0.8;
     public static double INTAKE_SPIN_POWER = 1.0;
-    public static double CLAW_CLOSE = 0.0;
-    public static double CLAW_OPEN = 0.4;
-    public static double WRIST_DOWN = 0.0;
-    public static double WRIST_BACK = 1.0;
-    public static double ARM_DOWN = 0.0;
-    public static double ARM_BACK = 1.0;
+    public static double UNLOCK = 0;
+
+    public static double LOCK = -0.5;
+
+
 
     // end of exposed variables
     @Override
@@ -53,21 +74,33 @@ public class ProdTeleOP extends LinearOpMode {
         intakeServoRight = hardwareMap.get(Servo.class, "intakeServoRight");
         intakeCRSLeft = hardwareMap.get(CRServo.class, "intakeCRSLeft");
         intakeCRSRight = hardwareMap.get(CRServo.class, "intakeCRSRight");
-        clawServo = hardwareMap.get(Servo.class, "clawServo");
+        fingerServo = hardwareMap.get(Servo.class, "fingerServo");
+        wristServo = hardwareMap.get(Servo.class, "wristServo");
+        armServo = hardwareMap.get(Servo.class, "armServo");
+        lockServo = hardwareMap.get(Servo.class, "lockServo");
+        outtakeDrive1 = hardwareMap.get(DcMotor.class, "outmoto1");
+        outtakeDrive2 = hardwareMap.get(DcMotor.class, "outmoto2");
         // Distance sensor object
-        DistanceSensor distanceSensor = hardwareMap.get(DistanceSensor.class, "sensor_distance");
+        // DistanceSensor distanceSensor = hardwareMap.get(DistanceSensor.class, "sensor_distance");
 
 
         FtcDashboard dashboard = FtcDashboard.getInstance();
 
         // Config motor
         intakeDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        outtakeDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         intakeDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         intakeDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        outtakeDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        outtakeDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
         while (opModeIsActive()) {
+            telemetry.addData("Vert Motor 1 Position", outtakeDrive1.getCurrentPosition());
+            telemetry.addData("Vert Motor 1 Power", outtakeDrive1.getPower());
+            telemetry.addData("Vert Motor 2 Power", outtakeDrive2.getPower());
+            telemetry.update();
             MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
             drive.setDrivePowers(new PoseVelocity2d(
                     new Vector2d(-gamepad1.left_stick_y, -gamepad1.left_stick_x),
@@ -77,47 +110,57 @@ public class ProdTeleOP extends LinearOpMode {
                 moveSlideToPosition(FULL_EXTENSION);
                 spinIntake();
                 moveIntakeDown();
+                lockServo.setPosition(LOCK);
             }
             if (gamepad1.right_bumper) {
                 moveSlideToPosition(HALF_EXTENSION);
                 spinIntake();
                 moveIntakeDown();
+                lockServo.setPosition(LOCK);
+
             }
             if (gamepad1.left_trigger > 0.5) {
                 retractSlide();
+                lockServo.setPosition(UNLOCK);
+
+
+                spinIntake();
+
+
             }
-            while (gamepad1.y) {
+            while (gamepad1.y && opModeIsActive()) {
                 Backspin();
             }
             if (gamepad1.b) {
-                clawServo.setPosition(CLAW_OPEN);
+                moveVertSlidesToPos(1000, 1.0);
+
+
             }
-            if (gamepad1.a) {
-                setWristDown();
-                setArmDown();
-                sleep(1000);
-                closeClaw();
-                setArmBack();
-                setWristBack();
-            }
-            double distanceInMM = distanceSensor.getDistance(DistanceUnit.MM);
+
+          //  double distanceInMM = distanceSensor.getDistance(DistanceUnit.MM);
 
             // Print the distance value (for debugging)
-            telemetry.addData("Distance", distanceInMM);
-            telemetry.update();
+//            telemetry.addData("Distance", distanceInMM);
+//            telemetry.update();
 
-            // Check if the distance is less than or equal to 12mm
-            if (distanceInMM <= DISTANCE_THRESHOLD_MM) {
-                setWristDown();
-                setArmDown();
-                sleep(1000);
-                closeClaw();
-                setArmBack();
-                setWristBack();
-            }
+//            // Check if the distance is less than or equal to 12mm
+//            if (distanceInMM <= DISTANCE_THRESHOLD_MM) {
+//                setWristDown();
+//                setArmDown();
+//                sleep(1000);
+//                closeClaw();
+//                setArmBack();
+//                setWristBack();
+//            }
 
 
             telemetry.addData("Slide Position", intakeDrive.getCurrentPosition());
+            intakeDrive.setPower(0);
+            outtakeDrive1.setPower(0);
+            outtakeDrive2.setPower(0);
+            intakeCRSLeft.setPower(0);
+            intakeCRSRight.setPower(0);
+            telemetry.addData("Status", "Stopped");
             telemetry.update();
         }
     }
@@ -126,15 +169,49 @@ public class ProdTeleOP extends LinearOpMode {
         intakeDrive.setTargetPosition(position);
         intakeDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         intakeDrive.setPower(1.0);
-        while (intakeDrive.isBusy() && opModeIsActive()) {
+
+        while (intakeDrive.isBusy() && opModeIsActive()) { // Add opModeIsActive() check
             telemetry.addData("I want to move to:", position);
-            telemetry.addData("I am at: ", intakeDrive.getCurrentPosition());
+            telemetry.addData("I am at:", intakeDrive.getCurrentPosition());
             telemetry.update();
         }
-        intakeDrive.setPower(0.0);
-    }
 
-    private void retractSlide() {
+        intakeDrive.setPower(0.0); // Stop motor after reaching the target
+    }
+    public void moveVertSlidesToPos(int targetPosition, double maxPower) {
+        fingerServo.setPosition(FINGERCLOSE);
+        armServo.setPosition(ARMVERTICAL);
+        outtakeDrive1.setTargetPosition(targetPosition);
+        outtakeDrive2.setTargetPosition(targetPosition);
+
+        outtakeDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        outtakeDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        outtakeDrive1.setPower(-maxPower);
+        outtakeDrive2.setPower(maxPower);
+
+        while (outtakeDrive1.isBusy() && outtakeDrive2.isBusy() && opModeIsActive()) {
+            telemetry.addData("Motor 1", outtakeDrive1.getCurrentPosition());
+            telemetry.addData("Motor 2", outtakeDrive2.getCurrentPosition());
+            telemetry.update();
+        }
+        wristServo.setPosition(WRISTSCORE);
+
+        fingerServo.setPosition(FINGEROPEN);
+        sleep(500);
+        wristServo.setPosition(WRIST90);
+        outtakeDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        outtakeDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        outtakeDrive1.setTargetPosition(targetPosition);
+        outtakeDrive2.setTargetPosition(targetPosition);
+        outtakeDrive1.setPower(VERTDOWNPOWER1);
+        outtakeDrive2.setPower(VERTDOWNPOWER2);
+        armServo.setPosition(ARMGRAB);
+
+
+
+}
+private void retractSlide() {
 
         intakeServoLeft.setPosition(INTAKE_UP_LPOSITION);
         intakeServoRight.setPosition(INTAKE_UP_RPOSITION);
@@ -152,7 +229,7 @@ public class ProdTeleOP extends LinearOpMode {
     }
 //    private void moveIntakeUp() {
 //       intakeServoLeft.setPosition(INTAKE_UP_LPOSITION);
-//        intakeServoRight.setPosition(INTAKE_UP_RPOSITION);
+//       intakeServoRight.setPosition(INTAKE_UP_RPOSITION);
 //    }
 
     private void spinIntake() {
@@ -167,22 +244,5 @@ public class ProdTeleOP extends LinearOpMode {
         intakeCRSLeft.setPower(0);
         intakeCRSRight.setPower(0);
     }
-    private void closeClaw() {
-        clawServo.setPosition(CLAW_CLOSE);
-    }
-    private void openClaw(){
-        clawServo.setPosition(CLAW_OPEN);
-    }
-    private void setWristDown() {
-        wristServo.setPosition(WRIST_DOWN);
-    }
-    private void setWristBack(){
-        wristServo.setPosition(WRIST_BACK);
-    }
-    private void setArmDown(){
-        armServo.setPosition(ARM_DOWN);
-    }
-    private void setArmBack(){
-        armServo.setPosition(ARM_BACK);
-    }
+
 }
