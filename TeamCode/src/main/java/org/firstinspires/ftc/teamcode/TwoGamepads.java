c8c57d0b397f143666ab56e95bd2aa579eabae5f
awdev <165525907+awdev1@users.noreply.github.com>
2024-12-28 01:18:21 -0800

    Attempted to add threading in 2gamepads.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoGamepads.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoGamepads.java
@@ -2,52 +2,107 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.PoseVelocity2d;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "ImprovedTeleOp", group = "Linear OpMode")
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TwoGamepads", group = "Linear OpMode")
 @Config
-public class maybe extends LinearOpMode {
+public class TwoGamepads extends LinearOpMode {
 
     // Hardware variables
     private DcMotor intakeDrive, outtakeDrive1, outtakeDrive2;
-    private Servo intakeServoLeft, intakeServoRight, armServo, fingerServo, wristServo, lockServo;
+    private Servo intakeServoLeft, intakeServoRight, lockServo;
     private CRServo intakeCRSLeft, intakeCRSRight;
-
+    AnalogInput axonServo = hardwareMap.get(AnalogInput.class, "axonServo");
+    AnalogInput axonServo2 = hardwareMap.get(AnalogInput.class, "axonServo2");
     // Constants
-    public static double ARM_GRAB = -0.3;
-    public static double WRIST_90 = 0.5;
-    public static double WRIST_SCORE = -0.5;
-    public static double FINGER_OPEN = 0.4;
-    public static double FINGER_CLOSE = 0;
-    public static int FULL_EXTENSION = 965;
-    public static int HALF_EXTENSION = 482;
     public static double INTAKE_SPIN_POWER = 1.0;
     public static double INTAKE_DOWN_LPOSITION = 0.58;
     public static double INTAKE_DOWN_RPOSITION = 0.4;
     public static double INTAKE_UP_LPOSITION = 0.18;
     public static double INTAKE_UP_RPOSITION = 0.8;
-    public static double OUTTAKE_UP_POSITION = 3800;
     public static double LOCK = -0.5;
     public static double UNLOCK = 0;
+    public static int FULL_EXTENSION = 965;
+    public static int HALF_EXTENSION = 482;
+    private volatile boolean isRunning = true;
 
     @Override
     public void runOpMode() throws InterruptedException {
         initializeHardware();
         FtcDashboard dashboard = FtcDashboard.getInstance();
 
+        // Start threads
+        Thread slideControlThread = new Thread(this::handleSlideControls);
+        Thread intakeControlThread = new Thread(t0his::handleIntakeControls);
+        Thread manualOverrideThread = new Thread(this::manualOverrideRetract);
+        Thread handleIntakeControls = new Thread(this::handleIntakeControls);
+        Thread handleSlideControls = new Thread(this::handleSlideControls);
+        Thread extendSlides = new Thread(this::extendSlides);
+        Thread retractSlides = new Thread(this::retractSlides);
+        Thread moveIntakeDown = new Thread(this::moveIntakeDown);
+        Thread moveIntakeUp = new Thread(this::moveIntakeUp);
+        Thread activateIntake = new Thread(this::activateIntake);
+        Thread backspinIntake = new Thread(this::backspinIntake);
+        Thread stopAllMotors = new Thread(this::stopAllMotors);
+    
+
+        slideControlThread.start();
+        intakeControlThread.start();
+        manualOverrideThread.start();
+        handleIntakeControls.start();
+        handleSlideControls.start();
+        extendSlides.start();
+        retractSlides.start();
+        moveIntakeDown.start();
+        moveIntakeUp.start();
+        activateIntake.start();
+        backspinIntake.start();
+        stopAllMotors.start();
+
         waitForStart();
 
         while (opModeIsActive()) {
-            // Drivetrain control
-            controlDrivetrain();
+            // Gamepad1: Primary controls (e.g., drivetrain)
+            // Gamepad2: Secondary controls (e.g., arm, intake, etc.)
 
-            // Mechanism controls
+            // debugging
+            telemetry.addData("Slide Position", intakeDrive.getCurrentPosition());
+            telemetry.addData("Outtake Motor 1 Pos", outtakeDrive1.getCurrentPosition());
+            telemetry.addData("Outtake Motor 2 Pos", outtakeDrive2.getCurrentPosition());
+            telemetry.update();
+        }
+
+        // Stop them threads when the opmode is stopped 
+        isRunning = false;
+        slideControlThread.join();
+        intakeControlThread.join();
+        stopAllMotors();
+    }
+
+    private void initializeHardware() {
+        intakeDrive = hardwareMap.get(DcMotor.class, "intakeDrive");
+        outtakeDrive1 = hardwareMap.get(DcMotor.class, "outmoto1");
+        outtakeDrive2 = hardwareMap.get(DcMotor.class, "outmoto2");
+        intakeServoLeft = hardwareMap.get(Servo.class, "intakeServoLeft");
+        intakeServoRight = hardwareMap.get(Servo.class, "intakeServoRight");
+        intakeCRSLeft = hardwareMap.get(CRServo.class, "intakeCRSLeft");
+        intakeCRSRight = hardwareMap.get(CRServo.class, "intakeCRSRight");
+        lockServo = hardwareMap.get(Servo.class, "lockServo");
+
+        intakeDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        outtakeDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intakeDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        intakeDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        outtakeDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        outtakeDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    private void handleSlideControls() {
+        while (isRunning) {
             if (gamepad1.right_trigger > 0.5) {
                 extendSlides(FULL_EXTENSION);
                 activateIntake();
@@ -62,65 +117,36 @@ public class maybe extends LinearOpMode {
                 lockServo.setPosition(LOCK);
             }
 
-            if (gamepad1.left_trigger > 0.5) {
+            if (gamepad1.left_trigger > 0.5 || gamepad2.left_trigger > 0.5) {
                 retractSlides();
                 lockServo.setPosition(UNLOCK);
                 activateIntake();
             }
+        }
+    }
 
-            if (gamepad1.y) {
+    private void handleIntakeControls() {
+        while (isRunning) {
+            if (gamepad2.y) {
                 backspinIntake();
             }
 
-            if (gamepad1.b) {
-                scoreBlock(3500, 1.0);
-            }
-            if (gamepad1.dpad_down) {
-
-                moveOuttakeSlides(20,-0.5);
-            }
-            if (gamepad1.dpad_up) {
-                manualOverrideRetract();
+            if (gamepad2.b) {
+                stopAllMotors();
             }
 
-            // Telemetry for debugging
-            telemetry.addData("Slide Position", intakeDrive.getCurrentPosition());
-            telemetry.addData("Outtake Motor 1 Pos", outtakeDrive1.getCurrentPosition());
-            telemetry.addData("Outtake Motor 2 Pos", outtakeDrive2.getCurrentPosition());
-            telemetry.update();
+            if (gamepad2.x) {
+                activateIntake();
+            }
+
+            if (gamepad2.dpad_down) {
+                moveIntakeDown();
+            }
+
+            if (gamepad2.dpad_up) {
+                moveIntakeUp();
+            }
         }
-
-        stopAllMotors();
-    }
-
-    private void initializeHardware() {
-        intakeDrive = hardwareMap.get(DcMotor.class, "intakeDrive");
-        outtakeDrive1 = hardwareMap.get(DcMotor.class, "outmoto1");
-        outtakeDrive2 = hardwareMap.get(DcMotor.class, "outmoto2");
-        intakeServoLeft = hardwareMap.get(Servo.class, "intakeServoLeft");
-        intakeServoRight = hardwareMap.get(Servo.class, "intakeServoRight");
-        intakeCRSLeft = hardwareMap.get(CRServo.class, "intakeCRSLeft");
-        intakeCRSRight = hardwareMap.get(CRServo.class, "intakeCRSRight");
-        armServo = hardwareMap.get(Servo.class, "armServo");
-        fingerServo = hardwareMap.get(Servo.class, "fingerServo");
-        wristServo = hardwareMap.get(Servo.class, "wristServo");
-        lockServo = hardwareMap.get(Servo.class, "lockServo");
-
-        intakeDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        outtakeDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        intakeDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        intakeDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        outtakeDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        outtakeDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    }
-
-    private void controlDrivetrain() {
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-        drive.setDrivePowers(new PoseVelocity2d(
-                new Vector2d(-gamepad1.left_stick_y, -gamepad1.left_stick_x),
-                -gamepad1.right_stick_x
-        ));
     }
 
     private void extendSlides(int targetPosition) {
@@ -144,31 +170,6 @@ public class maybe extends LinearOpMode {
             telemetry.update();
         }
         intakeDrive.setPower(0);
-    }
-
-    private void scoreBlock(int targetPosition, double maxPower) {
-
-        moveOuttakeSlides(targetPosition, maxPower);
-
-
-    }
-
-
-    private void moveOuttakeSlides(int targetPosition, double maxPower) { // example: moveOuttakeSlides(3800,1)
-        outtakeDrive1.setTargetPosition(targetPosition);
-        outtakeDrive2.setTargetPosition(-targetPosition);
-
-        outtakeDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        outtakeDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        outtakeDrive1.setPower(maxPower);
-        outtakeDrive2.setPower(maxPower);
-
-        while (outtakeDrive1.isBusy() && outtakeDrive2.isBusy() && opModeIsActive()) {
-            telemetry.addData("Outtake 1 Position", outtakeDrive1.getCurrentPosition());
-            telemetry.addData("Outtake 2 Position", outtakeDrive2.getCurrentPosition());
-            telemetry.update();
-        }
     }
 
     private void moveIntakeDown() {
